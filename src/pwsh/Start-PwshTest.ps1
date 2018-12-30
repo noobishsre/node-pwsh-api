2cc7aa5c37a3efa4e3e1c0fade4ed0859fb18010
noobishsre <noobishsre@gmail.com>
2018-12-29 21:24:33 -0600

    Update Start-PwshTest.ps1

--- src/pwsh/Start-PwshTest.ps1
+++ src/pwsh/Start-PwshTest.ps1
@@ -1,6 +1,39 @@
+Param(
+    [string]$ComputerName
+)
 Function Start-PwshTest
 {
+    Param(
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$true,
+        HelpMessage="LogMessage:")]
+	    [string] $ComputerName
+    )
+    $date = Get-date
+    [string]$ts = $date.ToString("MM-dd-yyyy-HH:MM:ss")
+
+    [psobject]$spoolerService = get-service | select Name, Status | where{$_.Name -match "spooler"}
+    
+    $connTestGood = Test-Connection -ComputerName T9779POS0001 -Count 1 -Quiet
+    $connTestBad = Test-Connection -Computername T9779POS0002 -Count 1 -Quiet
+
     $functionName = $MyInvocation.MyCommand.Name 
-    Return $path
+    [psobject]$outObj = @{
+        Date = $ts
+        ComputerName = $ComputerName
+        FunctionName = $functionName
+        SpoolerObj = @{
+            Name = $spoolerService.Name
+            Status = $spoolerService.Status
+        }
+        GoodConnTest = $connTestGood
+        BadConnTest = $connTestBad
+    }
+    if(Test-Path -Path c:\temp\pwshouttest.txt)
+    {
+        Remove-Item C:\temp\pwshouttest.txt -Force
+    }
+    $outObj | out-file c:\temp\pwshouttest.txt
+    Return $outObj | ConvertTo-JSON
 }
-Start-PwshTest
+Start-PwshTest -Computername $ComputerName
